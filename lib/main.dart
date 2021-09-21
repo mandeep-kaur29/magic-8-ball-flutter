be2a917f0b1c007088b17add3b8227e45181b564
mandeep-kaur29 <mandeepkour1131267@gmail.com>
2021-09-21 04:42:57 -0700

    magic_ball

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,48 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() => runApp(
       MaterialApp(
-        home: null,
+        home: BallPage(),
       ),
     );
+
+// ignore: use_key_in_widget_constructors
+class BallPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.blue[900],
+          title: Text("Ask Me Anything"),
+        ),
+        body: Ball(),
+        backgroundColor: Colors.blueGrey);
+  }
+}
+
+// ignore: use_key_in_widget_constructors
+class Ball extends StatefulWidget {
+  @override
+  _BallState createState() => _BallState();
+}
+
+class _BallState extends State<Ball> {
+  int ballNumber = 1;
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      // ignore: deprecated_member_use
+      child: TextButton(
+        onPressed: () {
+          setState(() {
+            ballNumber = Random().nextInt(5) + 1;
+          });
+        },
+        child: Image.asset("images/ball$ballNumber.png"),
+      ),
+    );
+  }
+}
